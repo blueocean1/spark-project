272a3dcbd26459415de7180f55edfda468746a6c
Andriy Bezpaliuk <andriybezpaliuk@gmail.com>
2025-02-26 11:47:44 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+
+import lib.Utils
+from lib.logger import Log4j
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: sbdl {local, qa, prod} {load date}: Arguments are missing")
+        sys.exit(-1)
+
+    job_run_env = sys.argv[1].upper()
+    load_date = sys.argv[2]
+
+    spark = lib.Utils.get_spark_session(job_run_env)
+    logger = Log4j(spark)
+
+    logger.info("Finished creating Spark Session")
